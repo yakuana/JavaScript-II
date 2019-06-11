42145fa01574d85ebc76f848019d11e143feae7b
Ya'Kuana Davis <ydavis@bowdoin.edu>
2019-06-11 16:10:56 -0500

    WIP: callbacks.js All problems complete.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -24,32 +24,70 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length); 
 }
+
+// getLength function invocation 
+getLength(items, function(length) {
+  console.log(length); 
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]); 
 }
+
+// last function invocation 
+last(items, function(lastItem) {
+  console.log(lastItem); 
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y); 
 }
+
+// sumNums function invocation 
+sumNums(3, 4, function(result) {
+  console.log(result); 
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y); 
 }
+
+// multiplyNums function invocation 
+multiplyNums(3, 4, function(result) {
+  console.log(result); 
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(list.includes(item)); 
 }
 
+// contains function invocation 
+contains('Pencil', items, function(bool) {
+  console.log(bool); 
+});
+
 /* STRETCH PROBLEM */
+
+const myItems = ['Pencil', 'Pencil', 'Notebook', 'Pencil', 'yo-yo', 'Gum'];
 
 function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  let idx = 0; 
+  return cb(array.filter((item) => array.indexOf(item, idx += 1) === -1)); 
 }
+
+// removeDuplicates function invocation 
+removeDuplicates(myItems, function(dupArray) {
+  console.log(dupArray); 
+});
