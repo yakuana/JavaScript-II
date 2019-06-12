c9b47b76e3689b7c3fec07fe118b6ca80970b8e9
Ya'Kuana Davis <ydavis@bowdoin.edu>
2019-06-11 21:51:49 -0500

    Completed all Challenges and Stretch Goals

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,49 +1,83 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
-const number = 22;
-
 function multiply48() {
-  console.log(number * 48); 
+  
+  const number = 22; 
+
+  // returns nameless function 
+  return (() => {
+    // returns number times 48 
+    return number * 48; 
+  })();  // used IIFE to call function 
+
 }; 
 
-// multiply48(); 
+console.log(multiply48()); // returns 1056 
+
+
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
+
 
 // ==== Challenge 2: Create a counter function ====
 
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  
   var count = 0; 
+
+  // creates and returns nameless function 
   return () => {
-    return count = count + 1; 
+    // returns count plus one 
+    return ++count; 
   }
+
 };
 
+// creates an instance of counter 
 const newCounter = counter();
-console.log(newCounter()); // 1
-console.log(newCounter()); // 2
 
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+console.log(newCounter()); // returns 1
+console.log(newCounter()); // returns 2
+
+
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-
-//INCOMPLETE
 
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
   var counter = 0; 
-  return  {
-    "increment": () => {return counter += 1}, 
-    "decrement": () => {return counter -= 1},
+
+  function createMethod() {
+    // returns an Object of two methods 
+    return {
+      "increment": () => {return ++counter}, // add one to counter 
+      "decrement": () => {return --counter}, // subtract one from counter 
+    }; 
   }
+
+  // calls and returns createMethod
+  return createMethod(); 
 };
 
+// instance of counterFactory
 const newCounterFactory = counterFactory();
-console.log(newCounterFactory); // 1
-console.log(newCounterFactory); // 2
+
+ // { increment: [Function: increment], decrement: [Function: decrement] }
+console.log(newCounterFactory);
+
+// 1 
+console.log(newCounterFactory.increment()); 
+
+// 2
+console.log(newCounterFactory.increment());
+
+// 1 
+console.log(newCounterFactory.decrement());
+
+// 0 
+console.log(newCounterFactory.decrement());
+
